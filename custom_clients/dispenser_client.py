2093d438a67482fdaa8b91da0ede4e3d922a18d7
klaus <klauszeng@gmail.com>
2018-08-09 00:07:52 -0400

    update dispenser clinet(not tested yet)

--- custom_clients/dispenser_client.py
+++ custom_clients/dispenser_client.py
@@ -1,73 +1,78 @@
-import sys, os, time, json
+import sys, os, time, json, threading, Queue, requests, picamera, io
 import numpy as np 
-import threading 
-import Queue
+import RPi.GPIO as GPI
 from helper_functions import proximity 
-import RPi.GPIO as GPIO
-import requests, picamera, io
 
-class PiClient: 
-    def __init__(self):
-        self.camera = picamera.PiCamera()
-        self.camera.resolution = (1920, 1088)
-        self.camera.start_preview()
-        time.sleep(2)
-        self.url = 'http://192.168.0.101:5000/sanushost/api/v1.0/sanitizer_img' ## Input
-        self.node_id = '1' ## Input
-        self.shape = '(1088, 1920, 3)' 
-        self.image = np.empty((1088, 1920, 3), dtype=np.uint8)
-    def capture_and_post(self):
-        timestamp = time.time()
-        # stream = io.BytesIO()
-        # self.camera.capture(stream, format='jpeg')
-        # stream.seek(0)
-        self.camera.capture(self.image, 'rgb')
-        image_temp = self.image.astype(np.float64)
-        image_64 = base64.b64encode(image_temp)
-        # image_64 = base64.b64encode(stream.getvalue()).decode('ascii')
-        payload = {'NodeID': self.node_id, 'Timestamp': timestamp, 'Image': image_64, 'Shape': self.shape}
-        headers = {'Content_Type': 'application/json', 'Accept': 'text/plain'}
-        result = requests.post(self.url, json=payload, headers=headers)
-        print(result)
-        return result
+class DispenserClient:
+	def __init__(self):
+		'''
+		Initialize all local variables and functions
+		'''
+		## camera 
+		self.camera = PiCamera.PiCamera()
+		self.camera.resolutions = (1920, 1088)
+		self.url = 'http://192.168.1.91:5000/sanushost/api/v1.0/sanitizer_img' ## Input
+		self.node_id = 'D1'
+		self.shape = '(1088, 1920, 3)' 
+		self.image = np.empty((1088, 1920, 3), dtype=np.uint8)
 
-'''
-This thread is for future use. We can also use this thread to control led light on demo day. 
+		## distance sensor
+		self.sensor = proximity.VL6180X(1)
 
+		## GPIO - LED 
+		self.GPIO.setwarnings(False)
+		self.GPIO.setmode(GPIO.BCM)
+		self.GPIO.setup(18, GPIO.OUT)
+		self.GPIO.setup(23, GPIO.OUT)
+
+		## temporary data structure for storage
+		self.work_queue =  Queue.Queue(10)
+		self.local_file_queue = Queue.Queue(10)
+
+		## alert thread instantiate 
+		self.alert = alert_thread("alert", work_queue)
+		self.alert.daemon = True 
+		self.alert.start()
+
+	def capture_and_post():
+	    # stream = io.BytesIO()
+	    # camera.capture(stream, format='jpeg')
+	    # stream.seek(0)
+	    self.camera.capture(self.image, 'rgb')
+	    image_temp = self.image.astype(np.float64)
+	    image_64 = base64.b64encode(image_temp)
+	    # image_64 = base64.b64encode(stream.getvalue()).decode('ascii')
+	    payload = {'NodeID': self.node_id, 'Timestamp': time.time(), 'Image': image_64, 'Shape': self.shape}
+	    headers = {'Content_Type': 'application/json', 'Accept': 'text/plain'}
+	    result = requests.post(self.url, json=payload, headers=headers)
+	    print(result)
+	    return result
+
+	def read_distance():
+		distance = self.sensor.read_distance() #return an int
+		if distance <= 100:
+			self.GPIO.output(18, True)
+			time.sleep(0.5)
+			self.GPIO.output(18, False)
+			#client.capture_and_post()
+		else:
+			self.GPIO.output(23, True)
+			time.sleep(0.5)
+			self.GPIO.output(23, False)
+			
 class alert_thread(threading.Thread):
 	def __init__(self, name, queue):
 		threading.Thread.__init__(self)
-		self.name = name
-		self.queue = queue
+		name = name
+		queue = queue
 	def run(self): ## Loop for alert thread
 		while 1:
-			if not self.queue.empty():
-				print self.queue.get()
-'''
-if __name__ == "__main__":
-	
-	## Initialize client
-	client = PiClient()
-	
-	## Distance sensors 
-	sensor = proximity.VL6180X(1)
-	sensor.read_distance()
-	
-	## LED
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(18, GPIO.OUT)
-	GPIO.setup(23, GPIO.OUT)
-	
-	## temporary data structure for storage
-	#work_queue =  Queue.Queue(10)
-	#local_file_queue = Queue.Queue(10)
-	
-	## alert thread instantiate 
-	#alert = alert_thread("alert", work_queue)
-	#alert.daemon = True 
-	#alert.start()
+			if not queue.empty():
+				print queue.get()
 
+if __name__ == "__main__":
+
+	client = DispenserClient()
 
 	while 1: 
 		''' Code for motion sensors 
@@ -86,27 +91,12 @@ if __name__ == "__main__":
 		# elif result == "Face detected":
 		# 	continue 
 		# elif result == "slow response time":
-		# 	result = requests.post(self.url, json=payload, headers=headers)
+		# 	result = requests.post(url, json=payload, headers=headers)
 		## code for try again, need to discuss this 
-
 
 		## temporary testing code 
 		#work_queue.put((time.time(), "message:" + str(time.time())))
 		#time.sleep(2)
-		
-		#read distance
-		a = sensor.read_distance()
-		print a 
-		if a <= 100:
-			GPIO.output(18, True)
-			time.sleep(0.5)
-			GPIO.output(18, False)
-			client.capture_and_post()
-		else:
-			GPIO.output(23, True)
-			time.sleep(0.5)
-			GPIO.output(23, False)
-			
 			
 		
 		
