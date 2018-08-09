cd1f98409c7e78e4942c17eed99147d29d948882
klaus <management@sanussolutions.com>
2018-08-09 14:08:52 -0400

    test1 finished"

--- custom_clients/dispenser_client.py
+++ custom_clients/dispenser_client.py
@@ -1,6 +1,6 @@
-import sys, os, time, json, threading, Queue, requests, picamera, io
+import sys, os, time, json, threading, queue, requests, io, base64, picamera
 import numpy as np 
-import RPi.GPIO as GPI
+import RPi.GPIO as GPIO
 from helper_functions import proximity 
 
 class DispenserClient:
@@ -9,94 +9,83 @@ class DispenserClient:
 		Initialize all local variables and functions
 		'''
 		## camera 
-		self.camera = PiCamera.PiCamera()
-		self.camera.resolutions = (1920, 1088)
-		self.url = 'http://192.168.1.91:5000/sanushost/api/v1.0/sanitizer_img' ## Input
+		self.camera = picamera.PiCamera()
+		self.camera.resolution = (1920, 1088) 
+		self.url = 'http://192.168.0.103:5000/sanushost/api/v1.0/sanitizer_img' ## Input
 		self.node_id = 'D1'
 		self.shape = '(1088, 1920, 3)' 
 		self.image = np.empty((1088, 1920, 3), dtype=np.uint8)
+		print('dispenser client camera initialized')
 
 		## distance sensor
 		self.sensor = proximity.VL6180X(1)
+		print('dispenser client TOF sensor initialized')
 
 		## GPIO - LED 
-		self.GPIO.setwarnings(False)
-		self.GPIO.setmode(GPIO.BCM)
-		self.GPIO.setup(18, GPIO.OUT)
-		self.GPIO.setup(23, GPIO.OUT)
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(18, GPIO.OUT)
+		GPIO.setup(23, GPIO.OUT)
+		print('dispenser client GPIO check')
 
 		## temporary data structure for storage
-		self.work_queue =  Queue.Queue(10)
-		self.local_file_queue = Queue.Queue(10)
+		self.work_queue =  queue.Queue(10)
+		self.local_file_queue = queue.Queue(10)
+		print('dispenser client message queue initialized')
 
 		## alert thread instantiate 
-		self.alert = alert_thread("alert", work_queue)
+		self.alert = alert_thread("alert", self.work_queue)
 		self.alert.daemon = True 
 		self.alert.start()
+		print('dispenser client alert thread initialized')
 
-	def capture_and_post():
-		# stream = io.BytesIO()
-		# camera.capture(stream, format='jpeg')
-		# stream.seek(0)
+	def capture_and_post(self):
+		self.camera.start_preview(fullscreen=False, window = (100,20,640,480))
+		time.sleep(2)
 		self.camera.capture(self.image, 'rgb')
+		self.camera.stop_preview()
 		image_temp = self.image.astype(np.float64)
 		image_64 = base64.b64encode(image_temp)
-		# image_64 = base64.b64encode(stream.getvalue()).decode('ascii')
 		payload = {'NodeID': self.node_id, 'Timestamp': time.time(), 'Image': image_64, 'Shape': self.shape}
 		headers = {'Content_Type': 'application/json', 'Accept': 'text/plain'}
 		result = requests.post(self.url, json=payload, headers=headers)
 		print(result)
 		return result
 
-	def read_distance():
-		distance = self.sensor.read_distance() #return an int
-		if distance <= 100:
-			self.GPIO.output(18, True)
-			time.sleep(0.5)
-			self.GPIO.output(18, False)
-			#client.capture_and_post()
-		else:
-			self.GPIO.output(23, True)
-			time.sleep(0.5)
-			self.GPIO.output(23, False)
-			
+	def read_distance(self):
+		return self.sensor.read_distance() #return an int
+		
 class alert_thread(threading.Thread):
 	def __init__(self, name, queue):
 		threading.Thread.__init__(self)
-		name = name
-		queue = queue
+		self.name = name
+		self.queue = queue
 	def run(self): ## Loop for alert thread
 		while 1:
-			if not queue.empty():
-				print queue.get()
+			if not self.queue.empty():
+				print(self.queue.get())
 
 if __name__ == "__main__":
-
+	#logging.basicConfig(filename='debug.log', level=logging.DEBUG)		
+		
 	client = DispenserClient()
-
-	while 1: 
-		''' Code for motion sensors 
-		if motion:
-			print 'taking pic'
-			result = client.capture_and_post ## need to wait until some sort of result
-			case 1 2 3 
-		'''
-		##  This conditionals will be put into capture and post method inside PiClient later
-		# if result == None:
-		# 	local_file_queue.add(payload)
-		# elif result == "over load":
-		# 	local_file_queue.add(payload)
-		# elif result == "No face":
-		# 	continue
-		# elif result == "Face detected":
-		# 	continue 
-		# elif result == "slow response time":
-		# 	result = requests.post(url, json=payload, headers=headers)
-		## code for try again, need to discuss this 
-
-		## temporary testing code 
-		#work_queue.put((time.time(), "message:" + str(time.time())))
-		#time.sleep(2)
+	print('dispenser client initialized successfully')
+	
+	#Main loop
+	while 1:
+		distance = client.read_distance()  ## sensor sometimes takes 2 loop cycle to get back to normal reading
+		print(distance)
+		if distance >= 100:
+			GPIO.output(18, True)
+			time.sleep(0.5)
+			GPIO.output(18, False)
+		elif distance >= 20:
+			GPIO.output(23, True)
+			time.sleep(0.5)
+			GPIO.output(23, False)
+			client.capture_and_post()
+			print ('capture successfully')
+		else:
+			sys.exit()
 			
-		
-		
+	sys.exit()
