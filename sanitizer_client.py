4df32ad48973f08f7f73446cdfbe5bb07f588a60
klaus <klauszeng@gmail.com>
2019-09-10 13:38:15 -0400

    rename client

---
+++ sanitizer_client.py
@@ -0,0 +1,96 @@
+import configparser, logging
+import queue, time, datetime, sys
+import pi_camera, cv2, base64
+import post_request_thread
+import requests, json
+import numpy as np
+import RPi.GPIO as GPIO
+
+try: 
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+except:
+        raise Exception("config.ini file missing.")
+        
+class SanitizerClient:
+    def __init__(self, ):
+
+        self.route = config.get('SERVER', 'Route')
+        self.init_logger()
+        self.init_camera()
+        self.init_ir_sensor()
+
+        ## post request thread instantiate 
+        self.unit = config.get('PROPERTY', 'Unit')
+        self.type = config.get('PROPERTY', 'Type')
+        self.node_id = config.get('PROPERTY', 'Id')
+        
+        ## message queue
+        self.message_queue = queue.Queue()
+
+        self.post_request_thread = post_request_thread.PostRequestThread("PostRequest", node_id, type, unit, self.message_queue)
+        self.post_request_thread.daemon = True 
+        self.post_request_thread.start()
+        self.logger.info('dispenser client post request thread initialized')
+
+    def init_logger(self, ):
+        ## Retrive parameters from configuration file
+        debug_level = config.get('DEBUG', 'LogLevel')
+        property_type = config.get('PROPERTY', 'Type')
+
+        ## Logger
+        if debug_level == 'Info':
+            level = logging.INFO
+        else:
+            level = logging.DEBUG
+
+        self.logger = logging.getLogger(property_type)
+        self.logger.setLevel(level)
+        ch = logging.StreamHandler()
+        #ch = logging.FileHandler('dispenser_client.log')
+        ch.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+    def init_camera(self, ):
+        ## Retrive parameters from configuration file 
+        resolution = config.get('CAMERA', 'Resolution')
+        node_id = config.get('PROPERTY', 'Id')
+        width = config.getint('CAMERA', 'Width')
+        height = config.getint('CAMERA', 'Height')
+        channel = config.getint('CAMERA', 'Channel')
+        rotation = config.getint('CAMERA', 'Rotation')
+
+        ## Camera
+        self.camera = pi_camera.PiCamera(rotation, resolution, (width, height, channel))
+        self.logger.info('dispenser client camera initialized, start_preview executed')
+
+    def init_ir_sensor(self, ):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.IN) #motion sensor
+        self.logger.info('dispenser client GPIO check')
+
+    def capture(self):
+        image_string, shape_string = self.camera.capture()
+        payload = {'NodeID': self.node_id, 'Timestamp': time.time() ,'Image': image_64, 'Shape': self.shape}
+        headers = {'Content_Type': 'application/json', 'Accept': 'text/plain'}
+        self.payload_queue.put((payload, headers, self.route)) # dispenser thread
+
+if __name__ == "__main__":
+
+    # Initialization
+    client = SanitizerClient()
+
+    #Main loop
+    while 1:
+        try:
+            if GPIO.input(4):
+                time.sleep(0.25)
+            else:
+                cur_time = time.time()
+                respond = client.capture()
+                time.sleep(2)
+        except KeyboardInterrupt:
+            sys.exit()
