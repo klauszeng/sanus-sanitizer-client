b893484209eb46a781e475700d3b99582b41e85c
klaus <klauszeng@gmail.com>
2019-09-09 22:29:47 -0400

    rename picamera

---
+++ pi_camera.py
@@ -0,0 +1,24 @@
+import picamera
+import cv2, base64
+import numpy as np
+
+class PiCamera:
+
+    def __init__(self, 
+        rotation = 0, 
+        resolution = None,
+        image_size = None,
+        ):
+
+        self.camera = picamera.PiCamera()
+        self.camera.rotatioon = rotation 
+        self.camera.resolution = resolution
+        self.image = np.empty(size, dtype = np.uint8)
+        self.camera.start_preview(fullscreen = False, window = (100,20,0,0))
+
+    def capture(self, ):
+        self.camera.capture(self.image, 'rgb')
+        shape_string = str(self.image.shape)
+        retval, buffer = cv2.imencode('.jpg', self.image)
+        image_string = base64.b64encode(buffer)
+        return image_string
