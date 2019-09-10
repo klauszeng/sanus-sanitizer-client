a69ae3aea8d08eb101411ded7ea45fb5ab55bad9
klaus <klauszeng@gmail.com>
2019-09-10 12:56:37 -0400

    fix logic errors

--- post_request_thread.py
+++ post_request_thread.py
@@ -1,13 +1,14 @@
 import threading
 import logging, configparser
-import Queue 
-import json
+import queue, time 
+import requests, json
+
 
 class PostRequestThread(threading.Thread):
-    def __init__(self, name, node_id, type, unit):
+    def __init__(self, name, node_id, type, unit, queue):
         ###########################################################################################
         # Logger
-        level = self.log_level(config.get('DEBUG', 'LogLevel'))
+        level = logging.DEBUG ## Temporary 
         self.logger = logging.getLogger('')   
         self.logger.setLevel(level)
         ch = logging.FileHandler('log/connection.log')
@@ -23,29 +24,34 @@ class PostRequestThread(threading.Thread):
         self.node_id = node_id
         self.type = type
         self.unit = unit
-        self.message_queue = Queue.Queue()
-
-        
-    def log_level(self, level):
-        if level == 'Info':
-            return logging.INFO
-        else:
-            return logging.DEBUG
+        self.message_queue = queue
 
     def run(self):
         while 1:
             if self.message_queue.qsize():
                 payload, headers, route = self.message_queue.get()
+                self.logger.info("Payload unpacked.")
+                print(payload, headers, route)
                 try:
                     result = requests.post(route, json=payload, headers=headers)
                     code = result.status_code
-                except Exception, e:
+                    if code != 200:
+                        self.logger.debug(code)
+                        with open('unsend_request.json', 'w') as f:
+                            f.write(json.dumps(payload))
+                except Exception as e:
                     self.logger.error("Unexpected error: " + str(e))
-                    with open('/unsend_requests.json', 'w') as f:
+                    with open('unsend_requests.json', 'w') as f:
                         f.write(json.dumps(payload))
 
 if __name__ == "__main__":
-    pt = PostRequestThread('test', 'test', 'test', 'test')
+    message_queue = queue.Queue()
+    pt = PostRequestThread('test', 'test', 'test', 'test', message_queue)
     pt.daemon = True 
     pt.start()
-    print(1)
+    url = 'http://192.168.1.101:5000/sanushost/api/v1.0/identify_face'
+    headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
+    payload = {"NodeID": "demo_entry", "Timestamp": time.time(), "Image": "None" , "Shape": "(480, 640, 3)"}
+    print(message_queue.put((payload, headers, url)))
+    time.sleep(2)
+    print(message_queue.put((1,2,3)))
